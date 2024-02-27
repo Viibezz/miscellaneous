9a901026fe7054b873355895c47590c61b34f826
Viibezz <brand@live.com>
2024-02-26 23:30:46 -0800

    Update packet_io.py

--- packet_io.py
+++ packet_io.py
@@ -6,7 +6,7 @@ packet_io.py
 The script utilizes IPv4 to bind a (TCP/UDP) socket to a specified  
 IP and port, to send & receive packets using the Socket library.
 The size of the socket buffer can be set to optimize packet handling. 
-The script tracks the throughput over time in a hashmap, to draw an 
+The script tracks the throughput over time in a set, to draw an 
 analysis diagram for analyzing a WiFi network's throughput at different 
 distances.
 
@@ -16,7 +16,7 @@ The throughput is calculated by dividing the size of each packet
 
 import socket
 import time
-
+import statistics
 
 class PacketHandler:
 
@@ -26,6 +26,8 @@ class PacketHandler:
         self.port = port
         self.packet_size = packet_size
         self.send_or_receive = send_or_receive
+        self.connected = False
+        self.throughputs = set()
 
         # Create a socket
         self.socket = socket.socket(
@@ -39,40 +41,55 @@ class PacketHandler:
     # This function sends packets
     def send_packet_client(self, packet):
         try:
-            self.socket.connect((self.ip, self.port))
+            start_time = time.time()
+            if not self.connected:
+                self.socket.connect((self.ip, self.port))
+                self.connected = True
             self.socket.sendall(bytes(packet, "utf-8"))
+            end_time = time.time()
+            print(self.display_throughput('uplink', end_time - start_time))
             data = self.socket.recv(self.packet_size)
-            print("The server echoes back: ", repr(data))
+            print("The server echoed back: ", repr(data))
         except Exception as e:
             print("Error: ", e)
 
     # This function receives packets
     def receive_packet_server(self):
         try:
-            # UDP is connectionless, Listen to TCP/IP with max 2 connections
+            # Listen to TCP/IP with max 2 connections 
             if self.protocol == "tcp":
                 self.socket.listen(2)
                 print(f"Server listening on {self.ip}:{self.port}...")
                 while True:
+                    start_time = time.time()
                     conn, addr = self.socket.accept()
                     with conn:
                         while True:
                             data = conn.recv(self.packet_size)
+                            end_time = time.time()
                             if not data:
                                 print(f"Connection closed by {addr}")
                                 break
-                            conn.sendall(data)
                             print(f"Connected by TCP/{addr} - Data received: {data}")
+                            conn.sendall(data)
+                            print('Echoed data back to client.')
+                            print(self.display_throughput('downlink', end_time - start_time))
+            # UDP is connectionless, no need to listen
             else:
                 while True:
+                    start_time = time.time()
                     data, addr = self.socket.recvfrom(self.packet_size)
+                    end_time = time.time()
                     print(f"From {addr} - Data received: {data}")
+                    print(self.display_throughput('downlink', end_time - start_time))
         except Exception as e:
             print("Error: ", e)
 
-    # This function displays throughput
-    def display_throughput(self):
-        pass
+    # This function displays (down/up)link throughput
+    def display_throughput(self, direction, elapsed_time):
+        tp = "{:.3f}".format((self.packet_size * .001) / elapsed_time)
+        throughputs.add(tp)
+        return f"Packet {direction} throughput: {tp} kilobytes per second"
 
 
 if __name__ == "__main__":
@@ -83,13 +100,28 @@ if __name__ == "__main__":
 
     # protocol, ip, port, packet_size = input("TCP or UDP: "), input('IP: '), input('Port: '), input('Packet Size: ')
 
-    protocol, ip, port, packet_size = "udp", "", 1337, 1024
+    protocol, ip, port, packet_size = "tcp", "", 1337, 1024
     packet_handler = PacketHandler(
         protocol.lower(), ip, port, packet_size, send_or_receive
     )
 
+    # send data to the server
     if send_or_receive == "1":
-        packet_data = input("Message to send: ")
-        packet_handler.send_packet_client(packet_data)
+        new_message = True
+        while new_message:
+            packet_data = input("Message to send (-1 to exit): ")
+            if packet_data != '-1':
+                packet_handler.send_packet_client(packet_data)
+            else:
+                new_message = False
+        print('Goodbye.')
+    # receive data
     elif send_or_receive == "2":
         received_packets = packet_handler.receive_packet_server()
+
+    print("Average throughput: ", statistics.mean(self.throughputs), 'kbps')
+
+
+    # 1) close the socket
+    # 2) Threading?
+    # 3) IPv6?
